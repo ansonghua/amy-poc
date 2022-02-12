ad2f7f9231b221e7883c165248eca5370bf9f382
Amy An <ansonghua@hotmail.com>
2022-02-12 09:32:52 -0600

    lambda

--- main.tf
+++ main.tf
@@ -1,16 +1,34 @@
-provider "aws" {
-    region = "us-east-1"
-}
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-amy"
-
-  tags = {
-    Name        = "My bkt"
-    Environment = "Dev"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
   }
+
+  required_version = "~> 1.0"
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "random_pet" "lambda_bucket_name" {
+  prefix = "learn-terraform-functions"
+  length = 4
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
+
+  acl           = "private"
+  force_destroy = true
 }
